2cda5915de4c88e1384c830ee98328ce84ec2f42
NutCMaster <nutcrackingmaster@gmail.com>
2026-03-24 17:08:51 +0200

    Fixed Source.lua
    now, not everyone in the server can ban people. ONLY YOU CAN >:)

--- source.lua
+++ source.lua
@@ -1,8 +1,19 @@
 local bannedPpl = {}
 
+local admins = {} -- admins go here. Example: {"Player", "QQQ"}
 
 
 game["Players"].PlayerAdded:Connect(function(player)
+    local IsAdmin = player.IsCreator 
+    if not IsAdmin then
+        for _,v in ipairs(admins) do
+            if v == player.UserID then
+                IsAdmin = true
+                break
+            end
+        end
+    end
+    if not IsAdmin then return end
     if bannedPpl[player.UserID ] then player:Kick("server banned, ;< ") return end
     player.Chatted:Connect(function (message, event)
         if string.find( message,";") and string.sub(message,1,4) == "kick" then
@@ -15,7 +26,7 @@ game["Players"].PlayerAdded:Connect(function(player)
             else
                 Chat:UnicastMessage("Player not found!", player)
             end
-        elseif string.find( message,";") and string.sub(message,1,9) == "ban" then
+        elseif string.find( message,";") and string.sub(message,1,9) == "serverban" then
             local argument = string.sub(message,11,string.len(message))
             print(argument)
             if game['Players'][argument] then
